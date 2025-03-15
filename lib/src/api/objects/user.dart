83941b854d2b5f6ae99581ddbc727aeae627273a
thmeofan <aezangdan@gmail.com>
2025-03-15 13:49:39 +0300

    jhjhjhjh

--- lib/src/api/objects/user.dart
+++ lib/src/api/objects/user.dart
@@ -14,7 +14,7 @@ class User extends TdObject {
     required this.status,
     this.profilePhoto,
     this.accentColorId,
-    required this.backgroundCustomEmojiId,
+    this.backgroundCustomEmojiId,
     required this.profileAccentColorId,
     required this.profileBackgroundCustomEmojiId,
     this.emojiStatus,
